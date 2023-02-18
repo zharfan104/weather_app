d22490b5d355c328c1370a3ae25db19d25528382
Zharfan Akbar <zharfan.akbar104@gmail.com>
2023-02-18 12:17:48 +0700

    Add WeatherRepository

---
+++ lib/modules/weather/repositories/weather_repository.dart
@@ -0,0 +1,20 @@
+import 'package:injectable/injectable.dart';
+
+import '../../../core/api/rest_client.dart';
+import '../../models/forecast_model.dart';
+import '../../models/weather_model.dart';
+
+@singleton
+class WeatherRepository {
+  WeatherRepository({
+    required this.restClient,
+  });
+
+  final RestClient restClient;
+
+  Future<ForecastModel> getForecast({required String city}) =>
+      restClient.getForecast(city);
+
+  Future<WeatherModel> getWeather({required String city}) =>
+      restClient.getWeather(city);
+}
